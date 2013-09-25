894e0954960b264ba6ae11cb1b705c073e9821f5
epaen <ep@protos.de>
2013-09-25 14:12:20 +0200

    added EPL 1.0 license to features, plugins, and xtext/xtend/java files in src folders, added feature information

--- src/plugins/de.protos.FlowOfWork.core.fow/xtend-gen/de/protos/FlowOfWork/core/fow/generator/FoWGenerator.java
+++ src/plugins/de.protos.FlowOfWork.core.fow/xtend-gen/de/protos/FlowOfWork/core/fow/generator/FoWGenerator.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2013 protos software gmbh (http://www.protos.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * CONTRIBUTORS:
+ * 		Thomas Schuetz (initial contribution)
+ */
 package de.protos.FlowOfWork.core.fow.generator;
 
 import com.google.inject.Inject;
